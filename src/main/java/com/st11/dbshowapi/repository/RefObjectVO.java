d69f712a72e1dbdb575daa21986d262c5c2f6b18
노영삼(Youngsam Roh)/DB개발팀/11ST <rohys03@sk.com>
2019-01-28 19:55:57 +0900

    ref Object Column Add

--- src/main/java/com/st11/dbshowapi/repository/RefObjectVO.java
+++ src/main/java/com/st11/dbshowapi/repository/RefObjectVO.java
@@ -6,6 +6,8 @@ import lombok.Data;
 public class RefObjectVO {
     private int depth;
     private String objectType;
+    private String owner;
+    private String status;
     private String objectName;
     private String referencedPath;
 }
